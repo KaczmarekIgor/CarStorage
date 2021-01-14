6308a2764843427f199d53a1347cab7fdbb4e0bd
igor_veolia <Igor009@interia.eu>
2021-01-14 22:54:15 +0100

    start build controller

--- src/main/java/pl/igor/demo/controller/CarController.java
+++ src/main/java/pl/igor/demo/controller/CarController.java
@@ -1,8 +1,24 @@
 package pl.igor.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pl.igor.demo.model.Car;
+import pl.igor.demo.service.CarServiceImpl;
+
+import java.util.List;
 
 @Controller
 public class CarController {
+
+    private CarServiceImpl carServiceImpl;
+    private List<Car> carList;
+
+
+    @Autowired
+    public CarController(CarServiceImpl carServiceImpl) {
+        this.carServiceImpl = carServiceImpl;
+        this.carList = carServiceImpl.getCarList();
+    }
+
 }
