e8f10c3b0dfda399d8e8d655fb478593c622ae7d
igor_veolia <Igor009@interia.eu>
2021-01-18 18:15:16 +0100

    start build controller

--- src/main/java/pl/igor/demo/controller/CarController.java
+++ src/main/java/pl/igor/demo/controller/CarController.java
@@ -3,6 +3,11 @@ package pl.igor.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.igor.demo.model.Car;
 import pl.igor.demo.service.CarServiceImpl;
 
@@ -20,5 +25,21 @@ public class CarController {
         this.carServiceImpl = carServiceImpl;
         this.carList = carServiceImpl.getCarList();
     }
+
+
+    @GetMapping("/cars")
+    public String getCars(Model model) {
+        model.addAttribute("cars", carList);
+        model.addAttribute("request", new Car());
+        return "car";
+    }
+
+    @PostMapping("/delete")
+    public String deleteCa(@RequestParam long id) {
+        carServiceImpl.deleteCarById(id);
+        return "redirect:/car";
+
+    }
+
 
 }
